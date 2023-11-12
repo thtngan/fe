c01330152f2f5eefc4e3fd746be4bdfbf9a66340
LuisVu <vudhung11@gmail.com>
2023-11-12 22:40:58 +0700

    Update LandingPage

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./Header.jsx";
+import MainHome from "./MainHome.jsx";
+
+function Home() {
+    return(
+    <>
+    <div className="home">
+        <Header/>
+        <MainHome/>
+    </div>
+    </>
+    );
+}
+
+
+
+export default Home;
