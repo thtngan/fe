52c5a027d9e75cc0acd351793edc19db36c8ea81
thtngan <ththaongan1303@gmail.com>
2023-11-13 20:22:42 +0700

    H: Homepage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Profile from "./components/Profile";
 import { Routes, Route, Link } from "react-router-dom";
 import SignIn from './components/SignIn';
+import Home from "./components/Home";
 
 
 function App() {
   return (
 
         <Routes>
-
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/profile" element={<Profile />} />
           {/* <Route exact path="/home" element={<Home/>} /> */}
           <Route exact path="/signin" element={<SignIn/>} />
